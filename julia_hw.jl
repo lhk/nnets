4ef6fec2c70029d10af619a59f70223e27f9d7ad
lars klein <lars.klein@rwth-aachen.de>
2016-09-18 20:30:11 +0200

    three layers, bad network

--- julia_hw.jl
+++ julia_hw.jl
@@ -35,14 +35,14 @@ function backward(layer::Lin, dy::arr)
   layer.W => dy * layer.x',
   layer.b => dy,
   )
-  return W'*dy, derivs
+  return layer.W'*dy, derivs
 end
 
 type SoftMaxLoss
   #state
   x::arr
   probs::arr
-  labels::arr # an array of indices, length is batch_size
+  labels::Int # an array of indices, length is batch_size
 
   SoftMaxLoss() = new()
 end
@@ -51,14 +51,16 @@ function forward!(layer::SoftMaxLoss, x::arr)
   layer.x=x
   exp_scores=exp(x)
   layer.probs=exp_scores/sum(exp_scores, 1)
-  return -log(layer.probs[layer.labels, collect(1:1:size(labels)[1])])
+  return -log(layer.probs)[layer.labels]
 end
 
 # attention, in most cases this will be the last node
 # here the backward pass actually starts
 # the value for d_y should be just ones
 function backward(layer::SoftMaxLoss, dy::arr)
-  return layer.probs - layer.labels, Dict()
+  dx=layer.probs
+  dx[layer.labels]-=1
+  return dx/10, Dict()
 end
 
 #computes a forward pass of a list of layers
@@ -101,44 +103,61 @@ end
 #creating the network
 
 offset=1
-scale=0.01
+scale=0.001
 
-W1=abs(randn(100, 784)+offset)*scale
-b1=abs(randn(100,1)+offset)*scale
+W1=abs(randn(200, 784)+offset)*scale
+b1=abs(randn(200,1)+offset)*scale
 
-W2=abs(randn(10,100)+offset)*scale
-b2=abs(randn(10,1)+offset)*scale
+W2=abs(randn(50, 200)+offset)*scale
+b2=abs(randn(50,1)+offset)*scale
+
+W3=abs(randn(10,50)+offset)*scale
+b3=abs(randn(10,1)+offset)*scale
 
 fc1=Lin(W1, b1)
 fc2=Lin(W2, b2)
+fc3=Lin(W3, b3)
 
 softmax=SoftMaxLoss()
 
 
-epochs=1
-features=1000
+epochs=30
+features=500
 
-alpha=1e-5
+alpha=1e-4
+lambda=1e-2
 
 for e=1:epochs
+  avg_loss=0
+  avg_hits=0
   for f=1:features
     in_data=trainfeatures(f)
     in_data=reshape(in_data, 784, 1)
     in_label=round(Int, trainlabel(f))
 
-    labels=zeros(10,1)
-    labels[in_label+1]=1
-    softmax.labels=labels
+    softmax.labels=in_label+1
 
     x1=forward!(fc1, in_data)
     x2=forward!(fc2, x1)
+    x3=forward!(fc3, x2)
 
-    out=forward!(softmax, x2)
+    predicted=indmax(x3)
+
+    if predicted==in_label+1
+      avg_hits+=1
+    end
+
+
+    out=forward!(softmax, x3)
+    avg_loss+=out
 
     dy=ones(10,1)
 
     dict=Dict()
     dy, temp_dict=backward(softmax, dy)
+    dict=merge(dict, temp_dict)
+
+    dy, temp_dict=backward(fc3, dy)
     dict=merge(dict, temp_dict)
 
     dy, temp_dict=backward(fc2, dy)
@@ -147,17 +166,23 @@ for e=1:epochs
     dy, temp_dict=backward(fc1, dy)
     dict=merge(dict, temp_dict)
 
-    fc1.W-=dict(fc1.W)*alpha
-    fc1.b-=dict(fc1.b)*alpha
-    fc2.W-=dict(fc2.W)*alpha
-    fc2.b-=dict(fc2.b)*alpha
+    fc1.W-=(dict[fc1.W] + norm(fc1.W)*lambda)*alpha
+    fc1.b-=(dict[fc1.b] + norm(fc1.b)*lambda)*alpha
+    fc2.W-=(dict[fc2.W] + norm(fc2.W)*lambda)*alpha
+    fc2.b-=(dict[fc2.b] + norm(fc2.b)*lambda)*alpha
+    fc3.W-=(dict[fc3.W] + norm(fc3.W)*lambda)*alpha
+    fc3.b-=(dict[fc3.b] + norm(fc3.b)*lambda)*alpha
 
-    print("finished training for")
-    print(e)
-    print(f)
-    println()
+    #println(f)
 
   end
+  avg_hits/=features
+  avg_loss/=features
+  println("average loss")
+  println(avg_loss)
+  println("average hits")
+  println(avg_hits)
+  println("--------------------")
 end
 
 println("finished")
